9afdbd6ce4f437f0d1178184dad2e0c0570efbe7
MichalLaptop <sowaa56@interia.pl>
2018-03-06 19:41:18 +0100

    Aktualizacja - problem z URL

--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -5,8 +5,30 @@ import viewPackage.JOptionPaneView;
 public class Main {
 
 	public static void main(String[] args) {
-		JOptionPaneView.menuStart();
+		boolean isFinished = true;
+		do {
+			int option = JOptionPaneView.menuStart();
 
+			switch (option) {
+			case 0:
+				break;
+
+			case 1:
+				break;
+
+			case 2:
+				JOptionPaneView.author();
+				break;
+
+			case 3:
+				isFinished = false;
+				break;
+
+			}
+
+		} while (isFinished);
+
+		System.exit(0);
 	}
 
 }
--- src/viewPackage/JOptionPaneView.java
+++ src/viewPackage/JOptionPaneView.java
@@ -2,10 +2,10 @@ package viewPackage;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.plaf.ColorUIResource;
@@ -13,36 +13,53 @@ import javax.swing.plaf.FontUIResource;
 
 import mainPackage.Main;
 
-public class JOptionPaneView extends JFrame {
+public class JOptionPaneView {
 
 	public static int menuStart() {
-		
-		
-		
+
 		UIManager UI = new UIManager();
 		ImageIcon icon = new ImageIcon(Main.class.getResource("/rollingdice.jpg"));
 		UI.put("OptionPane.background", new ColorUIResource(Color.white));
 		UI.put("Panel.background", new ColorUIResource(Color.white));
 		UI.put("OptionPane.messageForeground", Color.red);
 		UI.put("Button.background", Color.red);
-		UI.put("OptionPane.messageFont",new FontUIResource("Arial", Font.BOLD, 20));
-		
-		
-		
-		String menuText;
-		
-		
-		
+		UI.put("OptionPane.messageFont", new FontUIResource("Arial", Font.BOLD, 20));
 
-		int option =JOptionPane.showOptionDialog(null,"Rolling Dice Version 1.0"
-		, "RollingDice 1.0", JOptionPane.OK_CANCEL_OPTION,
-				JOptionPane.INFORMATION_MESSAGE, icon, new String[] { "Start game", "Rules","Author", "Exit" }, 
-				"default");
-		
-		
-		
+
+		int option = JOptionPane.showOptionDialog(null, "Rolling Dice Version 1.0", "RollingDice 1.0",
+				JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, icon,
+				new String[] { "Start game", "Rules", "Author", "Exit" }, "default");
+
 		return option;
 
 	}
+	
+	public static void author(){
+	
+		
+		UIManager UI = new UIManager();
+		UI.put("OptionPane.background", new ColorUIResource(Color.white));
+		UI.put("Panel.background", new ColorUIResource(Color.white));
+		UI.put("OptionPane.messageForeground", Color.red);
+		UI.put("Button.background", Color.red);
+		UI.put("OptionPane.messageFont", new FontUIResource("Arial", Font.BOLD, 20));
+		
+		URL domain = null;
+		try {
+			domain = new URL("http://example.com");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	
+
+		
+		JOptionPane.showMessageDialog(null,"Michal Sowinski\nEmail:sowinskim@interia.eu:\nGitHub"+ domain, "Author", JOptionPane.PLAIN_MESSAGE, null);
+		
+		
+	}
+	
+	
 
 }
